d35de225dc0893b7f29a037e8b08d075df460c32
kumarmudur <kumarmudur10@gmail.com>
2024-02-03 11:54:16 -0500

    [Task1] user list2 example

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 
-import UserList1 from './components/task1/UserList1';
+//import UserList1 from './components/task1/UserList1';
+import UserList2 from './components/task1/UsersList2';
 
 function App() {
   return (
     <div>
      <p>React Components</p>
-     <UserList1 />
+     <UserList2 />
     </div>
   )
 }
---
+++ src/components/task1/UsersList2.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const URL = 'https://jsonplaceholder.typicode.com/users';
+
+const UserList2 = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    const fetchUsers = () => {
+        axios.get(URL)
+            .then(response => setUsers(response.data))
+            .catch(error => console.error(error));
+    };
+
+    return (
+        <div style={{ textAlign: 'center'}}>
+            <h1>Users List 2 Details Table</h1>
+            <table style={{ borderCollapse: 'collapse', margin: 'auto', width: '100%', border: '1px solid #ddd'}}>
+                <thead>
+                   <tr style={{ border: '1px solid #ddd', backgroundColor: 'black', color: 'white'}}>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Mobile</th>
+                   </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map(({id, name, email, phone}) => (
+                            <tr key={id} style={{ border: '1px solid #ddd', backgroundColor: 'gray', color: 'white'}}>
+                                <td>{name}</td>
+                                <td>{email}</td>
+                                <td>{phone}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default UserList2;
