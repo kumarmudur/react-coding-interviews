5502f9c684d9cd63fdc3bdc12b4b26d54ec8fc7c
kumarmudur <kumarmudur10@gmail.com>
2024-02-14 21:16:55 -0500

    main file changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import DisplayModeProvider from './components/task10/DisplayModeProvider';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DisplayModeProvider>
+      <App />
+    </DisplayModeProvider>
   </React.StrictMode>,
 )
