f84aec838ee010a0aa4773d6982f8822012e6035
kumarmudur <kumarmudur10@gmail.com>
2024-03-10 16:21:15 -0400

    js project accordion

---
+++ src/js-projects/01-accordion/main.js
@@ -0,0 +1,36 @@
+import data from './data.js';
+
+const accordionWrapper = document.querySelector('.accordion');
+
+const createAccordionData = () => {
+    accordionWrapper.innerHTML = data.map(dataItem => `
+        <div class="accordion_item">
+            <div class="accordion_title">
+                <h3>${dataItem.question}</h3>
+                <i class="fa-solid fa-arrow-down"></i>
+            </div>
+            <div class="accordion_content">
+                <p>${dataItem.answer}</p>
+            </div>
+        </div>
+    `).join(' ');
+}
+
+createAccordionData();
+
+const accordionTitles = document.querySelectorAll('.accordion_title');
+
+accordionTitles.forEach((currentItem) => {
+    currentItem.addEventListener('click', () => {
+        if (currentItem.classList.contains('active')) {
+            currentItem.classList.remove('active');
+        } else {
+            let getAlreadyAddedActiveClasses = document.querySelectorAll('.active');
+            getAlreadyAddedActiveClasses.forEach(currentActiveItem => {
+                currentActiveItem.classList.remove('active');
+            });
+
+            currentItem.classList.add('active');
+        }
+    })
+});
