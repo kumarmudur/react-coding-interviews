d1155313b5bb62e27606ec886a30ac08b188c59b
kumarmudur <kumarmudur10@gmail.com>
2024-07-01 21:47:39 -0400

    [reactjs] use-fetch example

---
+++ src/react-projects/16-use-fetch/test.jsx
@@ -0,0 +1,24 @@
+import useFetch from ".";
+
+const UseFetchHookTest = () => {
+    const { data, error, pending} = useFetch('https://dummyjson.com/products', {});
+
+    console.log(data);
+    console.log(error);
+    console.log(pending);
+    return (
+        <div>
+            <h1>Use Fetch Hook</h1>
+            { pending ? <h3>Pending! Please wait</h3> : null }
+            { error ? <h3>{error}</h3> : null}
+            {
+                data && data.products && data.products.length ?
+                data.products.map((productItem) => (
+                    <p key={productItem.id}>{productItem.title}</p>
+                )) : null
+            }
+        </div>
+    );
+};
+
+export default UseFetchHookTest;
