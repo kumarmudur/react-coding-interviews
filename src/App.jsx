751397a85c5649b9f3c3ae2745f22f3444be3b3e
kumarmudur <kumarmudur10@gmail.com>
2024-08-10 21:46:40 -0400

    [Reactjs] count context example

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,72 @@ import './App.css'
 // import Pagination from './components/task8/Pagination';
 //import InfiniteScroll from './components/task9/InfiniteScroll';
 
-import Counter from './components/task11/Counter';
+//import Counter from './components/task11/Counter';
+// import Counter from './components/task12/Counter';
 
-import { useDisplayMode } from './components/task10/DisplayModeProvider';
+//import { useDisplayMode } from './components/task10/DisplayModeProvider';
+
+// Custom Hooks
+// import AppFocus from './components/customHooks/useFocus';
+
+// Questions
+//import CountState from './components/questions/CountState';
+
+// Design Patterns
+//import HOCApp from './designPattens/hoc/App';
+
+// React Projects
+// import Accoridian from './react-projects/01-accordion';
+// import RandomColor from './react-projects/02-random-color';
+// import StarRating from './react-projects/03-star-rating';
+// import ImageSlider from './react-projects/04-image-slider';
+// import LoadMoreData from './react-projects/05-load-more-data';
+// import TreeView from './react-projects/06-tree-view';
+// import menus from './react-projects/06-tree-view/data';
+// import QRCodeGenerator from './react-projects/07-qr-code-generator';
+// import LightDarkMode from './react-projects/08-light-dark-mode';
+// import ScrollIndicator from './react-projects/09-scroll-indicator';
+// import TabsTest from './react-projects/10-custom-tabs/tabs-test';
+// import ModalTest from './react-projects/11-custom-modal-pop/modal-test';
+// import GitHubProfileFinder from './react-projects/12-github-profile-finder';
+// import SearchAutoComplete from './react-projects/13-search-autocomplete';
+// import TicTacToe from './react-projects/14-tic-tac-toe';
+// import FeatureFlagsGlobalState from './react-projects/15-feature-flag/context';
+// import FeatureFlags from './react-projects/15-feature-flag';
+// import UseFetchHookTest from './react-projects/16-use-fetch/test';
+// import UseOnClickOutsideTest from './react-projects/17-use-outside-click/test';
+// import UseWindowResizeTest from './react-projects/18-use-window-resize/test';
+// import ScrollToTopAndBottom from './react-projects/19-scroll-to-top-and-bottom';
+// import ScrollToSection from './react-projects/19-scroll-to-top-and-bottom/scroll-to-section';
+// import Weather from './react-projects/20-weather-app/components/weather';
+// import MyComponent from './interviewQuestions/context';
+
+// import PaginationTest from './react-projects/21-pagination/test';
+// import DigitalClock from './react-projects/22-digital-clock';
+// import CountdownTimerTest from './react-projects/23-countdown-timer/test';
+// import StepProgressBarTest from './react-projects/24-step-progress-bar/test';
+// import RandomQuoteGenerator from './react-projects/25-random-quote-generator';
+// import TooltipTest from './react-projects/26-tooltip/test';
+// import CurrencyConverter from './react-projects/27-currency-converter';
+// import FilterProducts from './react-projects/28-filter-cards';
+// import TipCalculator from './react-projects/29-tip-calculator';
+// import MusicPlayer from './react-projects/30-music-player';
+// import Progressbar from './react-projects/31-custom-progress-bar';
+// import BMICalculator from './react-projects/32-bmi-calculator';
+import ButtonRipple from './react-projects/33-button-ripple-effect';
 
 function App() {
-  const { displayMode, toggleDisplayMode } = useDisplayMode();
+  // const { displayMode, toggleDisplayMode } = useDisplayMode();
 
-  const appStyle = {
-    background: displayMode === "light" ? "#ffffff" : "#333333",
-    color: displayMode === "light" ? "#333333" : "#ffffff",
-    padding: "5px",
-  };
+  // const appStyle = {
+  //   background: displayMode === "light" ? "#ffffff" : "#333333",
+  //   color: displayMode === "light" ? "#333333" : "#ffffff",
+  //   padding: "5px",
+  // };
 
   return (
-    <div>
-     <p>React Components</p>
+    <div className='App'>
+     {/* <p>React Components</p> */}
      {/* <UserList2 /> */}
      {/* <Search /> */}
      {/* <Todo /> */}
@@ -42,7 +92,58 @@ function App() {
         {displayMode === "light" ? "Dark Mode" : "Light Mode"}
       </button>
      </div> */}
-    <Counter />
+    {/* <Counter /> */}
+    {/* <AppFocus /> */}
+
+    {/* <CountState /> */}
+
+    {/* <HOCApp /> */}
+
+    {/* React Projects */}
+    {/* <Accoridian /> */}
+    {/* <RandomColor /> */}
+    {/* <StarRating /> */}
+    {/* <ImageSlider 
+       url={"https://picsum.photos/v2/list"}
+       page={"1"}
+       limit={"10"}
+    /> */}
+    {/* <LoadMoreData /> */}
+    {/* <TreeView menus={menus} /> */}
+    {/* <QRCodeGenerator /> */}
+    {/* <LightDarkMode /> */}
+    {/* <ScrollIndicator url={'https://dummyjson.com/products?limit=100'}/> */}
+    {/* <TabsTest /> */}
+    {/* <ModalTest /> */}
+    {/* <GitHubProfileFinder /> */}
+    {/* <SearchAutoComplete /> */}
+    {/* <TicTacToe /> */}
+
+    {/* <FeatureFlagsGlobalState>
+      <FeatureFlags />
+    </FeatureFlagsGlobalState> */}
+
+    {/* <UseFetchHookTest /> */}
+    {/* <UseOnClickOutsideTest /> */}
+    {/* <UseWindowResizeTest /> */}
+    {/* <ScrollToTopAndBottom /> */}
+    {/* <ScrollToSection /> */}
+    {/* <Weather /> */}
+    {/* <MyComponent /> */}
+
+    {/* <PaginationTest /> */}
+    {/* <DigitalClock /> */}
+    {/* <CountdownTimerTest /> */}
+    {/* <StepProgressBarTest /> */}
+    {/* <RandomQuoteGenerator /> */}
+    {/* <TooltipTest /> */}
+    {/* <CurrencyConverter /> */}
+    {/* <FilterProducts /> */}
+    {/* <TipCalculator /> */}
+    {/* <MusicPlayer /> */}
+    {/* <Progressbar /> */}
+    {/* <BMICalculator /> */}
+    <ButtonRipple />
     </div>
   )
 }
